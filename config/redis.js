d6cf0a95ea1786c9eb820d379a711f7565ce37a8
asm-dev <anasalamancamontero@gmail.com>
2025-06-06 21:49:09 +0100

    Fix: add Redis socket for production env

--- config/redis.js
+++ config/redis.js
@@ -3,8 +3,16 @@ import { createClient } from "redis";
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const redisClient = createClient({
   url: process.env.REDIS_URL || "redis://localhost:6379",
+  ...(isProduction && {
+    socket: {
+      tls: true,
+      rejectUnauthorized: false,
+    },
+  }),
 });
 
 redisClient.on("error", (err) => console.error("Redis Error:", err));
