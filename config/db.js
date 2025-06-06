b4753199aa9089537b7beecb11d9e0d90fff6afe
asm-dev <anasalamancamontero@gmail.com>
2025-06-06 15:11:27 +0100

    Fix: ensure variable name match Heroku DB postgre

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false, // Cambiar a true si se quiere ver las consultas SQL
 });
 
