7fd8877d4fb1ce4eb30ead784a78e934990feb29
asm-dev <anasalamancamontero@gmail.com>
2025-06-06 21:33:34 +0100

    Fix: define environment on sequelize init

--- config/db.js
+++ config/db.js
@@ -3,16 +3,24 @@ import { Sequelize } from "sequelize";
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
+if (!isProduction) {
+  dotenv.config();
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+  dialectOptions: isProduction
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
 });
 
 export default sequelize;
