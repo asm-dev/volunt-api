93c34ba86b866f2323fc863fd965949b74c1c09a
asm-dev <anasalamancamontero@gmail.com>
2025-06-06 21:22:32 +0100

    Specify dialect for Heroku/Postgres config

--- config/db.js
+++ config/db.js
@@ -4,7 +4,15 @@ import { Sequelize } from "sequelize";
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  logging: false, // Cambiar a true si se quiere ver las consultas SQL
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default sequelize;
